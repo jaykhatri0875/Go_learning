eea1a92cfa1bf9705548a35589429a18b0719818
jaykhatri0875 <jay.khatri.0875@ldce.ac.in>
2022-04-18 11:59:17 +0530

    first go example

---
+++ first.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	// this comment here
+	/* this is multiline comment here */
+	// go keywords are as below
+
+	var name = "jay khatri @author"
+	fmt.Println("YAY !! firt program from GO")
+	fmt.Printf("this is how you call variable : %s \n",name)
+
+}
